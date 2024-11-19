d8ffedb495322fc8def658c418958509b61b0a3f
Ibrahim <74654255+ibrahim317@users.noreply.github.com>
2024-11-20 01:04:32 +0200

    Update console.php to run scheduled tasks hourly

--- routes/console.php
+++ routes/console.php
@@ -4,5 +4,5 @@ use App\Console\Commands\NotifyUsers;
 use App\Console\Commands\MarkOverDue;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command(NotifyUsers::class)->everyMinute();
-Schedule::command(MarkOverDue::class)->everyMinute();
+Schedule::command(NotifyUsers::class)->hourly();
+Schedule::command(MarkOverDue::class)->hourly();
